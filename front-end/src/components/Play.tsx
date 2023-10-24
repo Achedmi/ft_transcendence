7f4e3a0e3eb09294f39c20722a3d7b61c0231725
ainzsoup <gamraouis@gmail.com>
2023-10-24 21:16:09 +0100

    ranking in progress page easter

--- front-end/src/components/Play.tsx
+++ front-end/src/components/Play.tsx
@@ -1,87 +1,107 @@
 import { Outlet } from "react-router-dom";
 import ChatButton from "../assets/chatButton.svg?react";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import { useState } from "react";
-import CloseChat from "../assets/closeChat.svg?react";
+
 const Play = () => {
+  const [isChatOpen, setIsChatOpen] = useState(false);
 
-    const [isChatOpen, setIsChatOpen] = useState(false);
+  return (
+    <div className="h-full w-full flex gap-4">
+      <div
+        className={
+          !isChatOpen
+            ? "bg-[#D9D9D9] border-solid border-dark-cl border-[4px] rounded-2xl h-full w-full flex justify-center items-center md:gap-24 gap-7 md:flex-row flex-col relative"
+            : "bg-[#D9D9D9] border-solid border-dark-cl border-[4px] rounded-2xl h-full w-1/2 flex justify-center items-center md:gap-24 gap-7  flex-col relative"
+        }
+      >
+        {!isChatOpen && (
+          <motion.div
+            onClick={() => setIsChatOpen(true)}
+            className="absolute top-[90%] right-10 hover:cursor-pointer"
+            whileHover={{ scale: 1.2 }}
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+          >
+            <ChatButton className="h-full w-full" />
+          </motion.div>
+        )}
 
-    return (
-        <div className="h-full w-full flex gap-4">
-            <div 
-                className={!isChatOpen ? "bg-[#D9D9D9] border-solid border-dark-cl border-[4px] rounded-2xl h-full w-full flex justify-center items-center md:gap-24 gap-7 md:flex-row flex-col relative" 
-                : "bg-[#D9D9D9] border-solid border-dark-cl border-[4px] rounded-2xl h-full w-1/2 flex justify-center items-center md:gap-24 gap-7  flex-col relative"}>
-                {!isChatOpen && (
-                        <motion.div
-                            onClick={() => setIsChatOpen(true)}
-                            className="absolute top-[90%] right-10 hover:cursor-pointer"
-                            whileHover={{ scale: 1.2  }}
-                            initial={{ opacity: 0 , scale: 0}}
-                            animate={{ opacity: 1 , scale: 1}}
-                            >
-                                <ChatButton className="h-full w-full"/>
-                        </motion.div>
-                )}
-                
-                <motion.div
-                    className={!isChatOpen ? "red relative aspect-[3/4] w-[40%]  md:w-[35%] max-w-[400px] border-solid border-[4px] border-dark-cl  flex flex-col hover:cursor-pointer"
-                                           : "w-3/4 red relative aspect-[3/4]  max-w-[250px] border-solid border-[4px] border-dark-cl  flex flex-col hover:cursor-pointer"}
-                    
-                    initial={{ rotate: 6 , y: -100, opacity: 0}}
-                    animate={{ y: 0 , opacity: 1}}
-                    whileHover={{ rotate: 0, y: 0, transition: { type: 'spring', stiffness: 300, damping: 15 } }}
-                    transition={{ type: 'spring', stiffness: 300, damping: 15 }}
-                    >
-                        <div className="bg-[#CACACA] rounded-full border-solid border-dark-cl border-[2px] md:border-[3px] absolute aspect-[1] h-[8%] top-[-5%] left-[50%]">
-                            <div className="bg-white rounded-full border-[2px] absolute aspect-[1] h-[30%] top-[5%] left-[15%]"></div>
-                        </div>
-                        <div className="bg-[#C84D46] aspect-w-4 aspect-h-5 h-[80%] border-b-[4px] border-solid border-dark-cl"></div>
-                        <div className="h-[20%] bg-white flex justify-center items-center text-xl md:text-2xl lg:text-3xl text-dark-cl">
-                            Classic Mode
-                        </div> 
-                </motion.div>
+        <motion.div
+          className={
+            !isChatOpen
+              ? "red relative aspect-[3/4] w-[40%]  md:w-[35%] max-w-[400px] border-solid border-[4px] border-dark-cl  flex flex-col hover:cursor-pointer"
+              : "w-3/4 red relative aspect-[3/4]  max-w-[250px] border-solid border-[4px] border-dark-cl  flex flex-col hover:cursor-pointer"
+          }
+          initial={{ rotate: 6, y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          whileHover={{
+            rotate: 0,
+            y: 0,
+            transition: { type: "spring", stiffness: 300, damping: 15 },
+          }}
+          transition={{ type: "spring", stiffness: 300, damping: 15 }}
+        >
+          <div className="bg-[#CACACA] rounded-full border-solid border-dark-cl border-[2px] md:border-[3px] absolute aspect-[1] h-[8%] top-[-5%] left-[50%]">
+            <div className="bg-white rounded-full border-[2px] absolute aspect-[1] h-[30%] top-[5%] left-[15%]"></div>
+          </div>
+          <div className="bg-[#C84D46] aspect-w-4 aspect-h-5 h-[80%] border-b-[4px] border-solid border-dark-cl"></div>
+          <div className="h-[20%] bg-white flex justify-center items-center text-xl md:text-2xl lg:text-3xl text-dark-cl">
+            Classic Mode
+          </div>
+        </motion.div>
 
-                    
-                <motion.div 
-                    className={!isChatOpen ? "blue relative aspect-[3/4] w-[40%]  md:w-[35%] max-w-[400px] border-solid border-[4px] border-dark-cl  flex flex-col hover:cursor-pointer"
-                                           : "w-3/4 blue relative aspect-[3/4]  max-w-[250px] border-solid border-[4px] border-dark-cl  flex flex-col hover:cursor-pointer"}
+        <motion.div
+          className={
+            !isChatOpen
+              ? "blue relative aspect-[3/4] w-[40%]  md:w-[35%] max-w-[400px] border-solid border-[4px] border-dark-cl  flex flex-col hover:cursor-pointer"
+              : "w-3/4 blue relative aspect-[3/4]  max-w-[250px] border-solid border-[4px] border-dark-cl  flex flex-col hover:cursor-pointer"
+          }
+          initial={{ rotate: -6, y: -100, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          whileHover={{
+            rotate: 0,
+            y: 0,
+            transition: { type: "spring", stiffness: 300, damping: 15 },
+          }}
+          transition={{ type: "spring", stiffness: 300, damping: 15 }}
+        >
+          <div className="bg-[#CACACA] rounded-full border-solid border-dark-cl border-[2px] md:border-[3px] absolute aspect-[1] h-[8%] top-[-5%] left-[30%]">
+            <div className="bg-white rounded-full border-[2px] absolute aspect-[1] h-[30%] top-[5%] left-[15%]"></div>
+          </div>
+          <div className="bg-blue-cl aspect-w-4 aspect-h-5 h-[80%] border-b-[4px]  border-solid border-dark-cl"></div>
+          <div className="h-[20%] bg-white flex justify-center items-center text-xl md:text-2xl lg:text-3xl text-dark-cl">
+            Powerups Mode
+          </div>
+        </motion.div>
+        <Outlet />
+      </div>
 
-                    initial={{ rotate: -6, y: -100, opacity: 0 }}
-                    animate={{ y: 0 , opacity: 1}}
-                    whileHover={{ rotate: 0, y: 0, transition: { type: 'spring', stiffness: 300, damping: 15 } }}
-                    >
-                        <div className="bg-[#CACACA] rounded-full border-solid border-dark-cl border-[2px] md:border-[3px] absolute aspect-[1] h-[8%] top-[-5%] left-[30%]">
-                            <div className="bg-white rounded-full border-[2px] absolute aspect-[1] h-[30%] top-[5%] left-[15%]"></div>
-                        </div>
-                        <div className="bg-blue-cl aspect-w-4 aspect-h-5 h-[80%] border-b-[4px]  border-solid border-dark-cl"></div>
-                        <div className="h-[20%] bg-white flex justify-center items-center text-xl md:text-2xl lg:text-3xl text-dark-cl">
-                            Powerups Mode
-                        </div>
-                </motion.div>
-                <Outlet/>
-            </div>
-            
-            {
-                isChatOpen && (
-                <motion.div className="w-1/2 bg-[#D9D9D9] border-solid border-dark-cl border-[4px] rounded-2xl relative "
-                            initial={{ opacity: 0 , scale: 0.5 }}
-                            animate={{ opacity: 1 , x: 0 , scale: 1, transition: { type: 'tween'} }}
-                            >
-                        <motion.div
-                            className=" bg-dark-cl h-8  w-8 absolute -top-4 -right-3 hover:cursor-pointer rounded-full flex justify-center items-center hover:bg-red-cl" onClick={() => setIsChatOpen(false)}
-                            whileHover={{ scale: 1.2  }}
-                            >
-                                <span className="text-white mt-1 text-2xl">X</span>
-                        </motion.div>
-                        
-                </motion.div>
-            )}
+      <AnimatePresence>
+        {isChatOpen && (
+          <motion.div
+            className="w-1/2 bg-[#D9D9D9] border-solid border-dark-cl border-[4px] rounded-2xl relative "
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{
+              opacity: 1,
+              x: 0,
+              scale: 1,
+              transition: { type: "tween" },
+            }}
+            exit={{ opacity: 0, scale: 0.5, transition: { type: "tween" } }}
+          >
+            <motion.div
+              className=" bg-dark-cl h-8  w-8 absolute -top-4 -right-3 hover:cursor-pointer rounded-full flex justify-center items-center hover:bg-red-cl"
+              onClick={() => setIsChatOpen(false)}
+              whileHover={{ scale: 1.2 }}
+            >
+              <span className="text-white mt-1 text-2xl">X</span>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
+    </div>
+  );
+};
 
-        </div>
-    )
-}
-
-
-
-export { Play};
+export { Play };
--- front-end/src/components/Ranking.tsx
+++ front-end/src/components/Ranking.tsx
@@ -1,9 +1,16 @@
+import { motion } from "framer-motion";
+
 const Ranking = () => {
-    return (
-        <div className="bg-red-300 h-[400px] w-[400px]">
-            <h1 className="text-3xl">Ranking</h1>
-        </div>
-    )
-}
+  return (
+    <div
+      className="flex justify-center items-center h-full w-full bg-[url('https://mcdn.wallpapersafari.com/medium/35/34/yfELA5.jpg')] bg-repeat 
+         border-solid border-dark-cl border-[4px] rounded-xl  "
+    >
+      <motion.div className="text-4xl text-white overflow-ellipsis">
+        🛠️ Work in progress... 🛠️
+      </motion.div>
+    </div>
+  );
+};
 
 export default Ranking;
