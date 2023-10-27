8b3ed1be3c0d0114ba94a37c065575ae72cf25bf
ainzsoup <gamraouis@gmail.com>
2023-10-27 01:26:17 +0100

    better error styling ?

--- front-end/src/components/LoginForm.tsx
+++ front-end/src/components/LoginForm.tsx
@@ -3,6 +3,7 @@ import UserIcon from "../assets/usericon.svg?react";
 import LockIcon from "../assets/lockicon.svg?react";
 import FtIcon from "../assets/fticon.svg?react";
 import { Link } from "react-router-dom";
+import { motion } from "framer-motion";
 
 import { useForm, SubmitHandler } from "react-hook-form";
 
@@ -38,7 +39,9 @@ function LoginForm() {
       <div className="flex flex-col items-center m-3 gap-2 w-[90%] h-full truncate">
         <div
           className={
-            "relative flex bg-light-gray-cl items-center border-solid border-[3px] border-dark-cl m-3 rounded-lg justify-start gap-2 h-16 my-5 w-full"
+            errors.username
+              ? "relative flex bg-light-gray-cl items-center border-solid border-[3px] border-red-cl m-3 rounded-lg justify-start gap-2 h-16 w-full "
+              : "relative flex bg-light-gray-cl items-center border-solid border-[3px] border-dark-cl m-3 rounded-lg justify-start gap-2 h-16 w-full"
           }
         >
           <UserIcon className="w-7 h-7 ml-1" />
@@ -54,7 +57,7 @@ function LoginForm() {
             })}
           />
           {errors.username && (
-            <span className="text-red-500 absolute -bottom-7 ">
+            <span className="text-red-cl absolute -bottom-7 ">
               {errors.username.type === "required"
                 ? "Username is required"
                 : errors.username.type === "maxLength"
@@ -76,7 +79,7 @@ function LoginForm() {
             {...register("password", { required: true, minLength: 8 })}
           />
           {errors.password && (
-            <span className="text-red-500 absolute -bottom-7">
+            <span className="text-red-cl absolute -bottom-7">
               {errors.password.type === "required"
                 ? "Password is required"
                 : errors.password.type === "minLength"
@@ -85,11 +88,11 @@ function LoginForm() {
             </span>
           )}
         </div>
-        <div className="bg-dark-cl m-3 h-16 rounded-lg w-full mt-12">
+        <motion.div className="bg-dark-cl m-3 h-16 rounded-lg w-full mt-12">
           <button type="submit" className="text-2xl text-white w-full h-full">
             sign in
           </button>
-        </div>
+        </motion.div>
         <Link
           to="/"
           className="flex  items-center justify-center bg-blue-cl m-3 h-16 rounded-lg border-[3px] border-solid gap-5 w-full"
