dc8fb063601b75eff24d73277396c14e6e7b59d0
ainzsoup <gamraouis@gmail.com>
2023-11-02 15:33:57 +0100

    ramdom user data generation and usage of react query

--- front-end/src/components/Profile.tsx
+++ front-end/src/components/Profile.tsx
@@ -1,16 +1,57 @@
 import { Edit } from "./icons/icons";
 import { motion } from "framer-motion";
+import axios from "axios";
+import { useQuery } from "react-query";
+
+const fetchProfile = async () => {
+  const response = await axios.get("https://randomuser.me/api/");
+  return response.data;
+};
 
 function Profile() {
+  const { data, isLoading } = useQuery("profile", fetchProfile);
+  type User = {
+    username: string;
+    picture: {
+      large: string;
+      medium: string;
+      small: string;
+    };
+    bio: string;
+    wins: number;
+    losses: number;
+  };
+  const imgSrc: string =
+    "https://i.pinimg.com/564x/90/74/c0/9074c097723d1832ea5c80cafa384104.jpg";
+
+  const user: User = {
+    username: isLoading ? "..." : data.results[0].login.username,
+    picture: {
+      large: isLoading ? imgSrc : data.results[0].picture.large,
+      medium: isLoading ? imgSrc : data.results[0].picture.medium,
+      small: isLoading ? imgSrc : data.results[0].picture.thumbnail,
+    },
+    bio: isLoading ? "..." : data.results[0].login.password,
+    wins: isLoading ? 0 : Math.floor(Math.random() * 100),
+    losses: isLoading ? 0 : Math.floor(Math.random() * 100),
+  };
+
   return (
     <div className="flex flex-col  bg-[#D9D9D9]  text-dark-cl border-solid border-dark-cl border-[4px] rounded-xl  h-full w-full">
-      <div className="bg-dark-cl h-48 relative">
+      <div className="bg-dark-cl h-48  relative">
         <motion.div>
-          <img
-            src="https://i.pinimg.com/564x/90/74/c0/9074c097723d1832ea5c80cafa384104.jpg"
-            alt=""
-            className="min-h-44 min-w-44 max-h-44 max-w-44 rounded-full absolute top-24 left-1/2 transform -translate-x-1/2 border-solid border-dark-cl border-[4px]"
-          />
+          {isLoading ? (
+            <div className="h-44 w-44 max-h-44 max-w-44 rounded-full overflow-hidden absolute top-24 left-1/2 transform -translate-x-1/2 border-solid border-dark-cl border-[4px] bg-dark-cl">
+              <div className="bg-[#D9D9D9] h-20 w-20 rounded-full absolute top-7 left-1/2 -translate-x-1/2"></div>
+              <div className="issue bg-[#D9D9D9] h-40 w-40 rounded-full absolute left-1/2 -translate-x-1/2 top-[70%] "></div>
+            </div>
+          ) : (
+            <img
+              src={user.picture.large}
+              alt=""
+              className="h-44 w-44 max-h-44 max-w-44 rounded-full absolute top-24 left-1/2 transform -translate-x-1/2 border-solid border-dark-cl border-[4px]"
+            />
+          )}
         </motion.div>
 
         <motion.div
@@ -23,19 +64,19 @@ function Profile() {
           <span className="hidden sm:block non-selectable">Edit profile</span>
         </motion.div>
       </div>
-      <div className="flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center ">
         <span className="text-3xl sm:text-4xl font-bold text-center mt-24">
-          ainzsoup
+          {user.username}
         </span>
         <div className="flex gap-8 w-full justify-center mt-8 sm:text-xl">
-          <span>666 Wins</span>
+          <span> {user.wins} Wins</span>
           <span>|</span>
-          <span>17 Losses</span>
+          <span> {user.losses} Losses</span>
         </div>
 
         <div className="BIO  h-16 w-[50%] bg-dark-cl border-solid border-dark-cl rounded-xl border-[4px] mt-14 relative flex justify-center items-center">
           <span className="absolute -top-8 left-0 text-xl">About me</span>
-          <span className="text-white text-sm sm:text-lg">Please be Patient i have autism .</span>
+          <span className="text-white text-sm sm:text-lg">{user.bio}</span>
         </div>
       </div>
     </div>
--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -1,27 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import Login from './components/Login.tsx'
-import Layout from './components/Layout.tsx'
-import Ranking from './components/Ranking'
-import Home from './components/Home.tsx'
-import Profile from './components/Profile.tsx'
-import  { Play }  from './components/Play'
-import { BrowserRouter as Router , Routes, Route } from "react-router-dom";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Login from "./components/Login.tsx";
+import Layout from "./components/Layout.tsx";
+import Ranking from "./components/Ranking";
+import Home from "./components/Home.tsx";
+import Profile from "./components/Profile.tsx";
+import { Play } from "./components/Play";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { QueryClientProvider, QueryClient } from "react-query";
 
+const queryClient = new QueryClient();
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login/>} />
-        <Route path="/" element={<Layout/> }>
-          <Route index element={<Home/>} />
-          <Route path="play" element={<Play/>}>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="play" element={<Play />}></Route>
+            <Route path="ranking" element={<Ranking />} />
+            dis
+            <Route path="profile" element={<Profile />} />
           </Route>
-          <Route path="ranking" element={<Ranking/>} />dis
-          <Route path="profile" element={<Profile/>} />
-        </Route>
-      </Routes>
-    </Router>
-  </React.StrictMode>,
-)
+        </Routes>
+      </Router>
+    </QueryClientProvider>
+  </React.StrictMode>
+);
