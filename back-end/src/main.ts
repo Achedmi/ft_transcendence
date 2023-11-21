8261610ffddff288bd4e1703ac5c8471fab6f24d
achedmi <abdekrime.chedmi@gmail.com>
2023-11-21 02:20:18 +0100

    adding data whitlisting

--- back-end/src/main.ts
+++ back-end/src/main.ts
@@ -2,6 +2,7 @@ import { HttpAdapterHost, NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { PrismaClientExceptionFilter } from './exceptions/all-exceptions.filter';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -13,7 +14,7 @@ async function bootstrap() {
   const { httpAdapter } = app.get(HttpAdapterHost);
   app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter));
   app.use(cookieParser());
-
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   await app.listen(4000);
 }
 bootstrap();
