1be64d69f5a63345396babce4db0571df20b8929
ainzsoup <gamraouis@gmail.com>
2023-12-10 12:22:01 +0100

    404 page

---
+++ front-end/src/components/pages/errorPages/NotFound404.tsx
@@ -0,0 +1,14 @@
+function NotFound404() {
+  return (
+	<div className="flex flex-col justify-center items-center h-screen font-Baloo font-bold ">
+	  <h1
+	  className="text-9xl text-dark-cl"
+	  >404</h1>
+	  <p
+	  className="text-4xl text-dark-cl"
+	  >Page not found</p>
+	</div>
+  );
+}
+
+export default NotFound404;
