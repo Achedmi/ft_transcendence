11f43ac742d28253cbf4b808a334938a8dbdc9d0
ainzsoup <gamraouis@gmail.com>
2023-12-10 12:20:32 +0100

    404 page on route

--- front-end/src/main.tsx
+++ front-end/src/main.tsx
@@ -14,6 +14,7 @@ import Users from "./components/pages/Users.tsx";
 import { Stats } from "./components/Stats.tsx";
 import Friends from "./components/pages/Profile/Friends.tsx";
 import React from "react";
+import NotFound404 from "./components/pages/errorPages/NotFound404.tsx";
 
 const queryClient = new QueryClient();
 
@@ -34,6 +35,7 @@ function App() {
 				<Route path="profile" element={<Profile />} />
 				<Route path="user/:username" element={<Users />} />
 			</Route>
+			<Route path="*" element={<NotFound404/>} />
 		</Routes>
 	);
 }
