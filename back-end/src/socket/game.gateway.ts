5697af44cff2c2749592111aa6a8016f66874384
achedmi <abdekrime.chedmi@gmail.com>
2023-12-20 13:18:43 +0100

    match making workflow almost done

--- back-end/src/socket/game.gateway.ts
+++ back-end/src/socket/game.gateway.ts
@@ -9,8 +9,22 @@ export class GameGateway {
   @WebSocketServer()
   server: Server;
 
-  readyToPlayQueue: {
-    [userId: number]: string;
+  private readyToPlayQueue = {};
+
+  private games: {
+    [gameId: number]: {
+      player1: {
+        userId: number;
+        socketId: string;
+        score: number;
+      };
+      player2: {
+        userId: number;
+        socketId: string;
+        score: number;
+      };
+      gameId: number;
+    };
   } = {};
 
   afterInit(server: Server) {
@@ -28,21 +42,55 @@ export class GameGateway {
   @SubscribeMessage('readyToPlay')
   async readyToPlay(client: Socket, data: { userId: number }) {
     console.log(client.id, 'joining game: ', data.userId);
-    this.readyToPlayQueue[data.userId] = client.id;
+    this.readyToPlayQueue[data.userId] = client;
     if (Object.keys(this.readyToPlayQueue).length >= 2) {
+      //create game in DB
       const gameId = (
         await this.gameService.create({
           players: Object.keys(this.readyToPlayQueue).map((userId) => +userId),
         })
       ).id;
-      const game = Object.keys(this.readyToPlayQueue).map((userId) => ({
-        userId: +userId,
-        socketId: this.readyToPlayQueue[userId],
+
+      //create game object
+      const game = {
+        player1: {
+          userId: +Object.keys(this.readyToPlayQueue)[0],
+          socketId: this.readyToPlayQueue[Object.keys(this.readyToPlayQueue)[0]],
+          score: 0,
+        },
+        player2: {
+          userId: +Object.keys(this.readyToPlayQueue)[1],
+          socketId: this.readyToPlayQueue[Object.keys(this.readyToPlayQueue)[1]],
+          score: 0,
+        },
         gameId,
-      }));
-      this.readyToPlayQueue = {};
-      client.join(String(gameId));
+      };
+
+      //joing the clients to the game room
+      this.readyToPlayQueue[game.player1.userId].join(String(gameId));
+      this.readyToPlayQueue[game.player2.userId].join(String(gameId));
       this.server.to(String(gameId)).emit('gameIsReady', game);
+
+      //save the game object in the games
+      this.games[gameId] = game;
+
+      //clear the readyToPlayQueue
+      this.readyToPlayQueue = {};
+
+      //start the game
+      this.startGame(this.games[gameId]);
     }
+  }
+
+  startGame(game) {
+    setInterval(() => {
+      this.server.to(String(game.gameId)).emit('gameUpdate', game);
+    }, 2000);
+  }
+
+  @SubscribeMessage('uncrementScore')
+  updateScore(client: Socket, data: { userId: number; score: number; gameId: number }) {
+    if (this.games[data.gameId].player1.userId === data.userId) this.games[data.gameId].player1.score++;
+    else this.games[data.gameId].player2.score++;
   }
 }
