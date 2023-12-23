fdc650ffa38fb9c4ae00453f086df6c461fedc27
ainzsoup <gamraouis@gmail.com>
2023-12-23 16:59:17 +0100

    visit player profile from match history

--- front-end/src/components/pages/Profile/MatchHistory.tsx
+++ front-end/src/components/pages/Profile/MatchHistory.tsx
@@ -1,14 +1,24 @@
 import { motion } from 'framer-motion';
 import axiosInstance from '../../../utils/axios';
 import { useQuery } from 'react-query';
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 function Row({ match }: any) {
-  let ongoing = match.status == 'ONGOING';
+  const [ongoing] = useState(match.status == 'ongoing');
+  const navigate = useNavigate();
+
+  const visitPlayer1Profile = useCallback(() => {
+    navigate(`/user/${match.player1.username}`);
+  }, []);
+
+  const visitPlayer2Profile = useCallback(() => {
+    navigate(`/user/${match.player2.username}`);
+  }, []);
   return (
     <motion.div className={'row  p-2  w-full border-b-4 border-solid border-dark-cl group hover:bg-dark-cl'}>
       <div className='flex justify-between items-center w-full group-hover:text-white'>
-        <div className='one w-[33%] flex flex-row flex-start gap-2 items-center'>
+        <div className='one w-[33%] flex flex-row flex-start gap-2 items-center cursor-pointer' onClick={visitPlayer1Profile}>
           <img
             className={
               'border-4 border-solid h-10 w-10 rounded-full aspect-square object-cover ' +
@@ -27,7 +37,7 @@ function Row({ match }: any) {
           </div>
         </div>
         <div className='two w-[33%] text-2xl flex justify-center group-hover:text-white'>{ongoing ? 'Ongoing' : `${match.player1Score} | ${match.player2Score}`}</div>
-        <div className='three  w-[33%] flex justify-end group-hover:text-white'>
+        <div className='three  w-[33%] flex justify-end group-hover:text-white cursor-pointer' onClick={visitPlayer2Profile}>
           <div className='flex flex-row gap-2 items-center group-hover:text-white'>
             <div
               className='player2 
@@ -64,13 +74,20 @@ export default function () {
   if (isLoading) {
     return <div className='w-full h-[85%]  flex items-center justify-center  '>Loading...</div>;
   }
+
   return (
     <div className='flex flex-col items-center  h-full w-full overflow-y-scroll scrollbar-thin scrollbar-thumb-[rgba(67,54,80,0.75)] scrollbar-thumb-rounded-full hover:scrollbar-thumb-dark-cl'>
-      <div className='flex flex-col items-center  w-full h-full '>
-        {data.map((game: any) => {
-          return <Row match={game} key={game.id} />;
-        })}
-      </div>
+      {data.length == 0 ? (
+        <div className='w-full h-[85%]  flex items-center justify-center  '>
+          <div className=' text-dark-cl'>No matches yet</div>
+        </div>
+      ) : (
+        <div className='flex flex-col items-center  w-full h-full '>
+          {data.map((game: any) => {
+            return <Row match={game} key={game.id} />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
