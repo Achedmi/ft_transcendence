2f9330cc186f9bf5514e81514de8886ccbb1776a
ainzsoup <gamraouis@gmail.com>
2023-12-24 11:25:14 +0100

    home responsivity patch

--- front-end/src/components/pages/Home.tsx
+++ front-end/src/components/pages/Home.tsx
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 function Home() {
   return (
     <div className='zwa9 bg-[#D9D9D9] border-solid border-dark-cl border-[4px] rounded-2xl flex items-center h-full justify-around '>
-      <div className='red flex flex-end h-full justify-center'>
+      <div className='red sm:flex flex-end h-full justify-center  hidden'>
         <motion.div
           className='w-full mt-48 max-h-[180px] h-[calc(100%-12rem)] hover:cursor-grab active:cursor-grabbing'
           drag='y'
@@ -20,7 +20,7 @@ function Home() {
       </div>
 
       <div className='pong flex  h-full flex-col  justify-center gap-6 items-center'>
-        <Link to='/play' className='w-full h-[20%]'>
+        <Link to='/play' className='w-72 sm:w-full  h-[20%]'>
           <motion.div className='h-full w-full' whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.5 }} initial={{ opacity: 0, scale: 0.5 }} animate={{ opacity: 1, scale: 1 }}>
             <HomePong className='w-full h-full' />
           </motion.div>
@@ -28,7 +28,7 @@ function Home() {
         <span className='text-xl text-dark-cl opacity-75'>click Pong to play</span>
       </div>
 
-      <div className='blue flex  items-end h-full justify-center '>
+      <div className='blue sm:flex  items-end h-full justify-center  hidden'>
         <motion.div
           className='w-full mb-48 max-h-[180px] h-[calc(100%-12rem)] hover:cursor-grab active:cursor-grabbing'
           drag='y'
